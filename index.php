88519e1d8bd67c8fa7697956124d73052950d0ca
Jack McConnell <hello@voltronik.co.uk>
2014-11-01 17:01:36 +0000

    Assets, sass, index.php

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
-<?php
-
-?>
 <!DOCTYPE html>
+
 <html>
 	<head>
+	    <meta charset="utf-8">
+
+		<!-- Mobile -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style.css">
+
         <title>Bath Alerts</title>
+
+        <!-- INSERT Google Analytics -->
     </head>
+
     <body>
+    	<header></header>
+
+    	<section id="postcode-form">
+    		<form action="">
+    			<input type="text" placeholder="BA1 5EB">
+    			<input type="submit" value="&f061;">
+    		</form>
+    	</section>
+
+    	<footer></footer>
     </body>
 </html>
