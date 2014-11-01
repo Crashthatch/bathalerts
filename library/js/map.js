4b606cf51b5d079c0382d1c88aaf50363fb4e67f
nadnerb33 <brendan@Brendans-MacBook-Pro.local>
2014-11-01 20:25:08 +0000

    changes to map.js

--- library/js/map.js
+++ library/js/map.js
@@ -9,45 +9,49 @@ var Acetate_all = L.tileLayer('http://a{s}.acetate.geoiq.com/tiles/acetate-hills
  var crimeMarker = [];
  var planningLayer = L.layerGroup()
  var planningMarker = [];
- var bounds = new L.LatLngBounds();
+ var propertyLayer = L.layerGroup()
+ var propertyMarker = [];
 
+ var bounds = new L.LatLngBounds();
+ var layersOnMap = [];
+ layersOnMap.push(Acetate_all);
+
+
+
+planningIcon  = L.AwesomeMarkers.icon({icon: 'fa-gavel', markerColor: 'red', prefix: 'fa'}) 
+crimeIcon     = L.AwesomeMarkers.icon({icon: 'fa-frown-o', markerColor: 'blue', prefix: 'fa'}) 
+propertyIcon  = L.AwesomeMarkers.icon({icon: 'fa-gbp', markerColor: 'green', prefix: 'fa'}) 
+postcodeIcon  = L.AwesomeMarkers.icon({icon: 'fa-home', markerColor: 'orange', prefix: 'fa'}) 
 
 $.each(crimeData, function(i, feature) {
 	//console.log(crime.location.latitude)
-    crimeMarker[i] = L.marker([feature.location.latitude, feature.location.longitude])
-                    .bindPopup('<b>Crime Type: </b>'+feature.crime_category+'<br>'+'<b>Location: </b>'+feature.street_name)
+    crimeMarker[i] = L.marker([feature.location.latitude, feature.location.longitude], {icon: crimeIcon})
+                    .bindPopup('<b>Date: </b>'+feature.month+'<br><b>Crime Type: </b>'+feature.crime_category+'<br>'+'<b>Location: </b>'+feature.street_name)
                     .addTo(crimeLayer);
     bounds.extend([feature.location.latitude, feature.location.longitude]);
+    layersOnMap.push(crimeLayer);
 });
 
 $.each(planningData, function(i, feature) {
-  //console.log(crime.location.latitude)
-    crimeMarker[i] = L.marker([feature.location.latitude, feature.location.longitude])
+    planningMarker[i] = L.marker([feature.location.latitude, feature.location.longitude], {icon: planningIcon})
                     .bindPopup('<b>Reference: </b>'+feature.casereference+'<br>'+'<b>Description: </b>'+feature.casetext)
                     .addTo(planningLayer);
     bounds.extend([feature.location.latitude, feature.location.longitude]);
+    layersOnMap.push(planningLayer);
 });
 
-
-
-//addDataToMap(planningData,planningLayer,"planning","red","feature.casetext");
-//addDataToMap(crimeData,crimeLayer,"crime","blue","crime_id");
-
-/*
-$.each(data, function(i, feature) {
-    planningMarker[i] = L.marker([feature.location.latitude, feature.location.longitude])
-                        .bindPopup()
-                        .addTo(layer);
+$.each(houseData, function(i, feature) {
+    propertyMarker[i] = L.marker([feature.location.latitude, feature.location.longitude], {icon: propertyIcon})
+                    .bindPopup('<b>Date of sale: </b>'+feature.date_of_transfer+'<br>'+'<b>Price:</b> £'+feature.price)
+                    .addTo(propertyLayer);
     bounds.extend([feature.location.latitude, feature.location.longitude]);
+    layersOnMap.push(propertyLayer);
 });
 
-*/
-
+L.marker([feature.location.latitude, feature.location.longitude], {icon: postcodeIcon}).bindPopup('Your Postcode').addTo(map);
 
     var map = L.map('map', {
-       //center: [51.38773470077388, -2.366824150085449],
-        //zoom: 15,
-        layers: [Acetate_all, crimeLayer, planningLayer]
+        layers: layersOnMap
     });
 
     var baseLayers = {
@@ -56,24 +60,12 @@ $.each(data, function(i, feature) {
 
     var overlays = {
         "Crimes": crimeLayer,
-        "Planning Applications": planningLayer
+        "Planning Applications": planningLayer,
+        "Property Sales": propertyLayer
     };
 
     L.control.layers(baseLayers, overlays).addTo(map);
 
     var highlight = L.geoJson(null).addTo(map);
     map.fitBounds(bounds);
-/*
 
-
-$.getJSON("assets/php/getMarkers.php", function (data) {
-         $.each(data.features, function(index, feature) {
-               if (feature.properties.Visited === 'true'){
-                   visitedMarkers.addData(feature);
-                   //console.log(index + ": visited"); 
-               }else{
-                   notVisitedMarkers.addData(feature);
-                   //console.log(index + ": not visited"); 
-               }
-         });
-*/
