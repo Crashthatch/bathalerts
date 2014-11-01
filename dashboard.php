fa37e06fd82c367d8183d250f77788072dbd848e
Jack McConnell <hello@voltronik.co.uk>
2014-11-01 17:58:45 +0000

    Completed home page

--- dashboard.php
+++ dashboard.php
@@ -11,7 +11,7 @@
 
 		<link rel="stylesheet" type="text/css" href="style.css">
 
-        <title>Bath Alerts - POST CODE</title>
+        <title>Bath Alerts</title>
 
         <!-- INSERT Google Analytics -->
     </head>
