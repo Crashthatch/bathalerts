9d50868e59928fe475c91e3232f4b97b53e952f6
Tom <fletcher.tom@gmail.com>
2014-11-01 17:44:20 +0000

    Add month params to filter to last 3 months of Crime data.

--- Modules/Crime.php
+++ Modules/Crime.php
@@ -11,10 +11,13 @@ class Crime extends Module {
         $postcodelat = self::$postCodeLoc[0];
         $postcodelong = self::$postCodeLoc[1];
 
-        $this->url .= '?$where=within_circle(location,'.$postcodelong.','.$postcodelat.',1000)';
+        $this->url .= '?$where=month>%272014-08-01%27%20AND%20month>%272014-11-01%27%20AND%20within_circle(location,'.$postcodelong.','.$postcodelat.',1000)';
     }
     
     function getData() {
+        //Find what's the latest crime known about in the dataset.
+
+
 
         $crimes = json_decode($this->fetch(), true);
         /*foreach($crimes as $crime) {
