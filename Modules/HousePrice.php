8587abcfae464fa8fab863777c6b1b884162f2bc
Tom <fletcher.tom@gmail.com>
2014-11-02 10:05:13 +0000

    Change houseprices to 6 months.

--- Modules/HousePrice.php
+++ Modules/HousePrice.php
@@ -7,10 +7,12 @@ class HousePrice extends Module {
     function getData() {
         $postcodelat = self::$postCodeLoc[0];
         $postcodelong = self::$postCodeLoc[1];
+
+        $startDate = date('Y-m-d', time()-6*30*24*60*60);
         $endDate = date('Y-m-d');
-        $startDate = date('Y-m-d', time()-3*30*24*60*60);
         $this->url .= '?$where=date_of_transfer>%27'.$startDate.'%27%20AND%20date_of_transfer<%27'.$endDate.'%27%20AND%20within_circle(location,'.$postcodelong.','.$postcodelat.',1000)';
 
+        echo $this->url;
 
 
         return json_decode($this->fetch(), true);
