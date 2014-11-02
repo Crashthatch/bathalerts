a3605179346c4dd8701f1fe57a3668b66c9d6ea7
Jack McConnell <hello@voltronik.co.uk>
2014-11-02 14:42:15 +0000

    Change dots for hyphens in assets

--- header.php
+++ header.php
@@ -7,19 +7,19 @@
 		<!-- CSS -->
 		<link rel="stylesheet" type="text/css" href="/library/css/style.css">
 		<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css">
-		<link rel="stylesheet" href="/bower_components/leaflet.awesome-markers/dist/leaflet.awesome-markers.css">
-		<link rel="stylesheet" href="/bower_components/leaflet.draw/dist/leaflet.draw.css">
+		<link rel="stylesheet" href="/bower_components/leaflet-awesome-markers/dist/leaflet.awesome-markers.css">
+		<link rel="stylesheet" href="/bower_components/leaflet-draw/dist/leaflet.draw.css">
 		<link rel="stylesheet" href="/library/css/map.css">
-		<link rel="stylesheet" href="/bower_components/leaflet.markerClusterer/dist/MarkerCluster.css">
-		<link rel="stylesheet" href="/bower_components/leaflet.markerClusterer/dist/MarkerCluster.Default.css">
+		<link rel="stylesheet" href="/bower_components/leaflet-markerClusterer/dist/MarkerCluster.css">
+		<link rel="stylesheet" href="/bower_components/leaflet-markerClusterer/dist/MarkerCluster.Default.css">
 
 		<!-- JS -->
 		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
-        <script src="/bower_components/leaflet.awesome-markers/dist/leaflet.awesome-markers.min.js"></script>
-        <script src="/bower_components/leaflet.draw/dist/leaflet.draw.js"></script>
-        <script src="/bower_components/leaflet.draw/src/edit/handler/Edit.Circle.js"></script>
-        <script src="/bower_components/leaflet.markerclusterer/dist/leaflet.markercluster.js"></script>
+        <script src="/bower_components/leaflet-awesome-markers/dist/leaflet.awesome-markers.min.js"></script>
+        <script src="/bower_components/leaflet-draw/dist/leaflet.draw.js"></script>
+        <script src="/bower_components/leaflet-draw/src/edit/handler/Edit.Circle.js"></script>
+        <script src="/bower_components/leaflet-markerclusterer/dist/leaflet.markercluster.js"></script>
         <script src="/library/js/scripts.min.js"></script>
 
         <?php if (isset($pc)) { ?>
