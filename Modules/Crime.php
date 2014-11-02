838f1889452c75f55a312a7c0fa44c79eb6be4da
Tom <fletcher.tom@gmail.com>
2014-11-02 11:56:42 +0000

    Make crimes date dynamic and sort by date DESC.

--- Modules/Crime.php
+++ Modules/Crime.php
@@ -7,7 +7,10 @@ class Crime extends Module {
     function getData() {
         $lat = $this->point->lat;
         $long = $this->point->long;
-        $this->url .= '?$where=month>%272014-08-01%27%20AND%20month<%272014-11-01%27%20AND%20within_circle(location,'.$long.','.$lat.',1000)';
+        $startDate = date('Y-m-d', time()-4*30*24*60*60);
+        $endDate = date('Y-m-d');
+
+        $this->url .= '?$where=month>%27'.$startDate.'%27%20AND%20month<%27'.$endDate.'%27%20AND%20within_circle(location,'.$long.','.$lat.',1000)&$order=month%20DESC';
         return json_decode($this->fetch(), true);
     }
 }
