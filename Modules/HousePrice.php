8f06fa23435eeddc376b9bf37e6f319460cd68a5
Tom <fletcher.tom@gmail.com>
2014-11-02 14:01:50 +0000

    Sort HousePrices New to Old.

--- Modules/HousePrice.php
+++ Modules/HousePrice.php
@@ -10,7 +10,7 @@ class HousePrice extends Module {
         $long = $this->point->long;
         $startDate = date('Y-m-d', time()-6*30*24*60*60);
         $endDate = date('Y-m-d');
-        $this->url .= '?$where=date_of_transfer>%27'.$startDate.'%27%20AND%20date_of_transfer<%27'.$endDate.'%27%20AND%20within_circle(location,'.$lat.','.$long.','.$this->radius.')';
+        $this->url .= '?$where=date_of_transfer>%27'.$startDate.'%27%20AND%20date_of_transfer<%27'.$endDate.'%27%20AND%20within_circle(location,'.$lat.','.$long.','.$this->radius.')&$order=date_of_transfer%20DESC';
         return json_decode($this->fetch(), true);
     }
 }
